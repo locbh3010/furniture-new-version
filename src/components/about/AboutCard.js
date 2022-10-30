53b9ae788eac25990896b8998e162c1836af3d3b
locbh3010 <104854529+locbh3010@users.noreply.github.com>
2022-10-30 20:09:30 +0700

    backup

--- src/components/about/AboutCard.js
+++ src/components/about/AboutCard.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { LazyLoadImage } from "react-lazy-load-image-component";
 
 const AboutCard = ({ member, delay }) => {
   const { avatar, name, position, number } = member;
@@ -7,20 +6,12 @@ const AboutCard = ({ member, delay }) => {
     <div className="flex flex-col items-stretch flex-1 w-full gap-12 ">
       <div className="rounded-[10px] relative flex-1 flex-shrink-0">
         <div className="w-full overflow-hidden rounded-[10px] h-[400px]">
-          <LazyLoadImage
-            src={avatar}
-            alt={avatar}
-            placeholderSrc={avatar}
-            width="100%"
-            height="100%"
-            className="object-cover w-full h-full rounded-[10px]"
-          />
-          {/* <img
+          <img
             src={avatar}
             alt={avatar}
             className="object-cover w-full h-full rounded-[10px]"
             loading="lazy"
-          /> */}
+          />
         </div>
         <div className="absolute bottom-0 -translate-x-1/2 translate-y-1/2 left-1/2">
           <img src={number} alt="" />
