3b9e2bb834f2974af1459bec73faf30ddff704a1
locbh3010 <locbh3010@gmail.com>
2022-11-22 16:18:33 +0700

    them blog o trang home

---
+++ src/components/blog/BlogUi.jsx
@@ -0,0 +1,80 @@
+import { collection, query, limit, onSnapshot } from "firebase/firestore";
+import React, { useRef } from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { db } from "../../configs/firebase.config";
+
+const BlogItem = ({ blog }) => {
+  const content = useRef(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    content.current.textContent = "";
+    content.current.insertAdjacentHTML("beforeend", blog.content);
+  }, [blog.content]);
+  const handleNavigate = navigate(`/blog/${blog.id}`);
+  return (
+    <div className="flex flex-col duration-300 bg-white rounded shadow group hover:shadow-lg">
+      <div
+        className="flex-shrink-0 overflow-hidden cursor-pointer aspect-video"
+        onClick={handleNavigate}
+      >
+        <img
+          src={blog.image}
+          alt=""
+          className="object-cover w-full h-full duration-300 group-hover:scale-125"
+        />
+      </div>
+      <div className="flex flex-col flex-1 px-5 py-4 border border-t-0 border-gray-300/30">
+        <h3
+          className="mt-3 mb-2 text-xl font-medium text-black cursor-pointer line-clamp-2"
+          onClick={handleNavigate}
+        >
+          {blog.name}
+        </h3>
+        <span className="mb-6 text-gray-500 line-clamp-3" ref={content}></span>
+        <div className="mt-auto">
+          <button
+            className="inline-flex items-center justify-center px-8 py-4 font-sans font-semibold tracking-wide text-white bg-blue-500 rounded cursor-pointer h-[60px] duration-300 hover:bg-blue-400"
+            onClick={handleNavigate}
+          >
+            Xem blog
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default function BlogList({ limitQuery = 3 }) {
+  const blogRef = collection(db, "blogs");
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    if (limitQuery !== 0) {
+      const blogRef_ = query(blogRef, limit(limit));
+      onSnapshot(blogRef_, (res) => {
+        const docs = res.docs;
+        let temp = [];
+        docs?.length > 0 &&
+          docs.map((doc) => temp.push({ id: doc.id, ...doc.data() }));
+        setBlogs(temp);
+      });
+    } else {
+      onSnapshot(blogRef, (res) => {
+        const docs = res.docs;
+        let temp = [];
+        docs?.length > 0 &&
+          docs.map((doc) => temp.push({ id: doc.id, ...doc.data() }));
+        setBlogs(temp);
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return (
+    <div className="grid grid-cols-3 gap-4 gird-flow-row auto-rows-fr">
+      {blogs?.length > 0 &&
+        blogs.map((blog) => <BlogItem key={blog.id} blog={blog} />)}
+    </div>
+  );
+}
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,6 +4,7 @@ import Hero from "../components/hero/Hero";
 import Pledge from "../components/section/Pledge";
 import ProjectHome from "../components/section/ProjectHome";
 import AboutHomeDesign from "../components/section/AboutHomeDesign";
+import BlogList from "../components/blog/BlogUi";
 
 const HomePage = () => {
   return (
@@ -12,6 +13,12 @@ const HomePage = () => {
       <AboutHome />
       <Pledge />
       <ProjectHome />
+      <div className="container py-12 mt-12">
+        <h2 className="mb-12 text-2xl font-bold text-center md:text-3xl">
+          Blog của Chúng tôi
+        </h2>
+        <BlogList />
+      </div>
       <AboutHomeDesign />
     </div>
   );
